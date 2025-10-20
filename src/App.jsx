7394406d1bad0b47ff1388fb9df62483996b11de
sahil-sth <saheel.sth99@gmail.com>
2025-10-20 12:53:50 -0600

    introduce AppNav for navigation in AppLayout

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
+        <Route path="/app" element={<AppLayout />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="*" element={<PageNotFound />} />
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,11 @@
 import PageNav from "../components/PageNav";
-
+import { Link } from "react-router-dom";
 function Homepage() {
   return (
     <div>
       <PageNav />
       <h1>Welcome to world wise</h1>
+      <Link to="/app">Go to the app</Link>
     </div>
   );
 }
