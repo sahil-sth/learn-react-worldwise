bb0dc43269618de1424f7b95813a680845c76c12
sahil-sth <saheel.sth99@gmail.com>
2025-10-21 20:08:21 -0600

    Add Map

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,9 +1,12 @@
 import Sidebar from "../components/Sidebar";
+import Map from "../components/Map";
 import styles from "./AppLayout.module.css";
+
 function AppLayout() {
   return (
     <div className={styles.app}>
       <Sidebar />
+      <Map />
     </div>
   );
 }
