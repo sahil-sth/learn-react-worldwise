aa1208365db0352af1eceb84d2a7100e2e8c4683
sahil-sth <saheel.sth99@gmail.com>
2025-10-21 23:15:04 -0600

    show a message when there are no cities

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,8 +1,11 @@
 import styles from "./CityList.module.css";
 import Spinner from "./Spinner";
 import CityItem from "./CityItem";
+import Message from "./Message";
 function CityList({ cities, isLoading }) {
   if (isLoading) return <Spinner />;
+  if (!cities.length)
+    return <Message message="No cities yet. Please add a city from the map." />;
   return (
     <ul className={styles.cityList}>
       {cities.map((city) => (
