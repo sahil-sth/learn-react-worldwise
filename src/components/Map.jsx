d13b2ed3d6ddcc70613ec235b6e437cc17ac091a
sahil-sth <saheel.sth99@gmail.com>
2025-10-22 23:10:41 -0600

    add lat lng search params

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,6 +1,18 @@
+import { useSearchParams } from "react-router-dom";
+
 import styles from "./Map.module.css";
 function Map() {
-  return <div className={styles.mapContainer}></div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+  return (
+    <div className={styles.mapContainer}>
+      <h1>Map</h1>
+      <h1>
+        Lat: {lat}, Lng: {lng}
+      </h1>
+    </div>
+  );
 }
 
 export default Map;
