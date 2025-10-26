67f91f3e3673b97b8d268fafdbe57822f9f3131d
sahil-sth <saheel.sth99@gmail.com>
2025-10-26 12:07:36 -0600

    set the city data by using getCity from CityProvider (CityContext)

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -7,6 +7,7 @@ const CitiesContext = createContext();
 function CitiesProvider({ children }) {
   const [cities, setCities] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [currentCity, setCurrentCity] = useState({});
   // as soon as it mounts
   useEffect(function () {
     async function fetchCities() {
@@ -23,8 +24,21 @@ function CitiesProvider({ children }) {
     }
     fetchCities();
   }, []);
+
+  async function getCity(id) {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_API_URL}/cities/${id}`);
+      const data = await res.json();
+      setCurrentCity(data);
+    } catch (e) {
+      console.error("Error while fetching city from API: " + e.message);
+    } finally {
+      setIsLoading(false);
+    }
+  }
   return (
-    <CitiesContext.Provider value={{ cities, isLoading }}>
+    <CitiesContext.Provider value={{ cities, isLoading, currentCity, getCity }}>
       {children}
     </CitiesContext.Provider>
   );
