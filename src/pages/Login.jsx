8867251c19b145e99824cd000891777008b20e3d
sahil-sth <saheel.sth99@gmail.com>
2025-11-04 21:47:29 -0700

    add login functionality with the help of AuthContext

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,39 @@
+import { useEffect, useState } from "react";
+import { useAuth } from "../contexts/AuthContext";
+
 import styles from "./Login.module.css";
-import { useState } from "react";
 import PageNav from "../components/PageNav";
+import { useNavigate } from "react-router-dom";
+
+import Button from "../components/Button";
 export default function Login() {
   // PRE-FILL FOR DEV PURPOSES
   const [email, setEmail] = useState("jack@example.com");
   const [password, setPassword] = useState("qwerty");
+  const navigate = useNavigate();
+  const { login, isAuthenticated } = useAuth();
 
+  useEffect(
+    function () {
+      console.log("useEffect called");
+      if (isAuthenticated) {
+        navigate("/app", { replace: true });
+      }
+    },
+    [isAuthenticated, navigate]
+  );
+
+  function handleLogin(e) {
+    e.preventDefault();
+    console.log("Button clicked");
+    if (email && password) {
+      login(email, password);
+    }
+  }
   return (
     <main className={styles.login}>
       <PageNav />
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleLogin}>
         <div className={styles.row}>
           <label htmlFor="email">Email address</label>
           <input
@@ -31,7 +55,7 @@ export default function Login() {
         </div>
 
         <div>
-          <button>Login</button>
+          <Button type="primary">Login</Button>
         </div>
       </form>
     </main>
