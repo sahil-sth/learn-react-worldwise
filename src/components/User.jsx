8a6ac499365bbc4dc44463beb0f139e26be3a9e6
sahil-sth <saheel.sth99@gmail.com>
2025-11-04 21:51:30 -0700

    add logout functionality with the help of AuthContext

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,16 +1,22 @@
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
+
 import styles from "./User.module.css";
 
-const FAKE_USER = {
-  name: "Jack",
-  email: "jack@example.com",
-  password: "qwerty",
-  avatar: "https://i.pravatar.cc/100?u=zz",
-};
+// const FAKE_USER = {
+//   name: "Jack",
+//   email: "jack@example.com",
+//   password: "qwerty",
+//   avatar: "https://i.pravatar.cc/100?u=zz",
+// };
 
 function User() {
-  const user = FAKE_USER;
-
-  function handleClick() {}
+  const { user, logout, isAuthenticated } = useAuth();
+  const navigate = useNavigate();
+  function handleClick() {
+    logout();
+    navigate("/");
+  }
 
   return (
     <div className={styles.user}>
